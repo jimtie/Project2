11aeb377582c36d3d14de402225208191394f07c
jimtie <jimmychen.xin@gmail.com>
2020-04-10 11:40:53 -0700

    template

---
+++ .env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jimmychen/code/work/sei-09/fundamantals/HW/Project2/.env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
