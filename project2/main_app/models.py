7b1d87b5dd9888acfe41b97392037f2856b4edcd
jimtie <jimmychen.xin@gmail.com>
2020-04-13 12:13:52 -0700

    changes

--- project2/main_app/models.py
+++ project2/main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Photo(models.Model):
@@ -22,6 +23,8 @@ class Album(models.Model):
 	name = models.CharField(max_length=100)
 	description = models.TextField(max_length=250)
 	photos = models.ManyToManyField(Photo)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.name
+
--- project2/main_app/views.py
+++ project2/main_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from django.http import HttpResponse
 from .models import Album, Photo
-# from .forms import AlbumForm, PhotoForm
+from .forms import AlbumForm, PhotoForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 
@@ -35,13 +35,36 @@ def assoc_photo(request, album_id, photo_id):
   return redirect('photos_detail', album_id=album_id)
 	
 
+# def album_createalbum(request):
+# 	# if request.method == 'POST':
+# 	form = AlbumForm(request,POST)
+# 	if form.is_valid():
+# 		new_album = form.save(commit = False)
+# 		new_album_id = album_id
+# 		new_album.save()
+# 	return redirect('albums', album_id = album_id)
+
 def album_createalbum(request):
-	form = AlbumForm(request,POST)
-	if form.is_valid():
-		new_album = form.save(commit = False)
-		new_album_id = album_id
-		new_album.save()
-	return redirect('albums', album_id = album_id)
+    # If a post request is made to this view function
+    if request.method == 'POST':
+        # We save the form data to a new variable
+        form = AlbumForm(request.POST)
+        # We make sure the data passes validations
+        if form.is_valid():
+            # If it does, associate album with logged in user and 
+            # save it in the database
+            album = form.save(commit=False)
+            album.user = request.user
+            album.save()
+            # Redirect the user to the new album's detail page
+            return redirect('albums_detail', album.id)
+    else:
+        # If it's a get request, load the form from forms.py
+        form = AlbumForm()
+    # Save the form to a new variable
+    context = { 'form': form }
+    # Render the album form template with the form
+    return render(request, 'albums/album_form.html', context)
 
 def photo_createphoto(request):
 	form = PhotoForm(request,POST)
